1da70caf84a046589a4983356e8b72e75491161d
Kinan-absar <kinan@absar-alomran.com>
2025-12-09 10:21:47 +0300

    redirect from py

--- employee_portal_suite/controllers/portal_sign_documents.py
+++ employee_portal_suite/controllers/portal_sign_documents.py
@@ -22,3 +22,25 @@ class EmployeePortalSignDocs(CustomerPortal):
         return request.render("employee_portal_suite.portal_sign_documents_page", {
             "sign_items": sign_items
         })
+# ---------------------------------------------------------
+# SIGN REDIRECT OVERRIDE (IMPORTANT)
+# ---------------------------------------------------------
+
+class SignRedirectOverride(Sign):
+
+    @http.route(
+        ['/sign/document/<int:request_id>/<string:access_token>/complete'],
+        type='http', auth='public', website=True, csrf=False
+    )
+    def sign_validate(self, request_id, access_token, **post):
+
+        # Run Odoo’s original signature completion logic
+        res = super(SignRedirectOverride, self).sign_validate(
+            request_id, access_token, **post
+        )
+
+        # If Odoo wants to redirect to /my/signature/<id>, override it
+        if isinstance(res, http.RedirectResponse) and "/my/signature/" in res.location:
+            return request.redirect("/my/employee/sign")
+
+        return res
