1a2cb47ebe050a84dc6b18ecc457cfaea68117f0
Kinan-absar <kinan@absar-alomran.com>
2025-12-09 11:22:32 +0300

    clean summary

--- employee_portal_suite/__manifest__.py
+++ employee_portal_suite/__manifest__.py
@@ -103,6 +103,7 @@
         'views/portal_material_approvals_list.xml',
         'views/portal_manager_request_detail.xml',
         'views/portal_sign_documents.xml',
+        'views/sign_portal_clean.xml',
 
     ],
 
