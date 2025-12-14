0ff907552f7b7fcf1c4a37c3b31362766a7468ad
Kinan-absar <kinan@absar-alomran.com>
2025-12-14 22:02:58 +0300

    sdf

--- petty_cash_management/models/res_config_settings.py
+++ petty_cash_management/models/res_config_settings.py
@@ -1,27 +1,24 @@
 from odoo import models, fields
 
-class PettyCashSettings(models.TransientModel):
-    _name = "petty.cash.settings"
-    _description = "Petty Cash Settings"
+class PettyCashConfig(models.Model):
+    _name = 'petty.cash.config'
+    _description = 'Petty Cash Configuration'
 
     petty_cash_account_id = fields.Many2one(
         'account.account',
         string="Petty Cash Account",
-        config_parameter='petty_cash_management.petty_cash_account_id'
+        required=True
     )
 
     input_vat_account_id = fields.Many2one(
         'account.account',
         string="Input VAT Account",
-        config_parameter='petty_cash_management.input_vat_account_id'
+        required=True
     )
 
     petty_cash_journal_id = fields.Many2one(
         'account.journal',
         string="Petty Cash Journal",
-        config_parameter='petty_cash_management.petty_cash_journal_id'
+        domain="[('type','=','general')]",
+        required=True
     )
-
-    def action_save(self):
-        # config_parameter fields are saved automatically
-        return {'type': 'ir.actions.act_window_close'}
