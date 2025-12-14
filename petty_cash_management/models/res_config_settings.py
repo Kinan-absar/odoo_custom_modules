90f0dfa9804e32bf656e0ebbca38bb44bf390259
Kinan-absar <kinan@absar-alomran.com>
2025-12-14 22:32:49 +0300

    restoring the one in master

---
+++ petty_cash_management/models/res_config_settings.py
@@ -0,0 +1,23 @@
+from odoo import models, fields
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    petty_cash_account_id = fields.Many2one(
+        'account.account',
+        string="Petty Cash Account",
+        config_parameter='petty_cash_management.petty_cash_account_id'
+    )
+
+    input_vat_account_id = fields.Many2one(
+        'account.account',
+        string="Input VAT Account",
+        config_parameter='petty_cash_management.input_vat_account_id'
+    )
+
+    petty_cash_journal_id = fields.Many2one(
+        'account.journal',
+        string="Petty Cash Journal",
+        domain="[('type','=','general')]",
+        config_parameter='petty_cash_management.petty_cash_journal_id'
+    )
