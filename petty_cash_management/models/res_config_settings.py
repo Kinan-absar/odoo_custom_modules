2b7ed14f89d518147659a164b558a79decd0e560
Kinan-absar <kinan@absar-alomran.com>
2025-12-14 22:11:27 +0300

    sdf

--- petty_cash_management/models/res_config_settings.py
+++ petty_cash_management/models/res_config_settings.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class PettyCashConfig(models.Model):
     _name = 'petty.cash.config'
@@ -9,16 +9,21 @@ class PettyCashConfig(models.Model):
         string="Petty Cash Account",
         required=True
     )
-
     input_vat_account_id = fields.Many2one(
         'account.account',
         string="Input VAT Account",
         required=True
     )
-
     petty_cash_journal_id = fields.Many2one(
         'account.journal',
         string="Petty Cash Journal",
         domain="[('type','=','general')]",
         required=True
     )
+
+    @api.model
+    def get_singleton(self):
+        config = self.search([], limit=1)
+        if not config:
+            config = self.create({})
+        return config
