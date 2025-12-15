0504ac7db867ab5f3e31265576e42217618080bb
Kinan-absar <kinan@absar-alomran.com>
2025-12-15 12:30:44 +0300

    removing models attendance

--- employee_portal_suite/models/__init__.py
+++ employee_portal_suite/models/__init__.py
@@ -1,7 +1,5 @@
 from . import employee_request
-from . import attendance_geo
 from . import approval_flow
-from . import work_location_inherit
 from . import material_request_line
 from . import material_request
 from . import purchase_order
