a8672f5c5d6e952a6f56ae8eea995c10862e752a
Kinan-absar <kinan@absar-alomran.com>
2025-12-15 11:34:05 +0300

    removing attendance

--- employee_portal_suite/controllers/__init__.py
+++ employee_portal_suite/controllers/__init__.py
@@ -1,6 +1,5 @@
 from . import portal_main
 from . import portal_request
-from . import portal_attendance
 from . import portal_redirect
 from . import portal_material_request
 from . import portal_sign_documents
